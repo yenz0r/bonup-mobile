a15454f7850e8373cf9b6bffad3a92686eecf5ea
ebychkovsky <ebychkovsky@gmail.com>
2020-03-08 01:30:14 +0300

    implemented localization molude

---
+++ bonup-mobile/bonup-mobile/Extensions/Localization/Localization.swift
@@ -0,0 +1,17 @@
+//
+//  Localization.swift
+//  bonup-mobile
+//
+//  Created by yenz0redd on 08.03.2020.
+//  Copyright © 2020 Bonup. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+
+}
