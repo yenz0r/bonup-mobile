62094eb8d13be50896180b122e5d6028bb07562a
ebychkovsky <ebychkovsky@gmail.com>
2020-03-08 00:16:02 +0300

    initial commit

---
+++ bonup-mobile/bonup-mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bonup-mobile
+//
+//  Created by yenz0redd on 08.03.2020.
+//  Copyright © 2020 Bonup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
